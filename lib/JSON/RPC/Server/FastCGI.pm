31c3df8b575e64b76cbc39345d343438ac87b748
Faiz Kazi <faiz@cpan.org>
2008-03-02 04:45:14 +0000

    Import of FAIZ/JSON-RPC-Server-FastCGI-0.03 from CPAN.
    gitpan-cpan-distribution: JSON-RPC-Server-FastCGI
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         FAIZ/JSON-RPC-Server-FastCGI-0.03.tar.gz
    gitpan-cpan-author:       FAIZ
    gitpan-cpan-maturity:     released

--- lib/JSON/RPC/Server/FastCGI.pm
+++ lib/JSON/RPC/Server/FastCGI.pm
@@ -11,11 +11,11 @@ JSON::RPC::Server::FastCGI - A FastCGI version of JSON::RPC::Server
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
